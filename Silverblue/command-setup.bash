4b33d3b4e6ae0128003edec8949cd9fe96b6789a
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-11-21 15:01:54 -0800

    cleanup

--- Silverblue/command-setup.bash
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-sudo rpm-ostree install --allow-inactive \
-  git-lfs \
-  kernel-tools \
-  powertop \
-  python3-speedtest-cli \
-  vim-enhanced
--- Silverblue/hosting-setup.bash
+++ Silverblue/hosting-setup.bash
@@ -1,14 +1,11 @@
 #! /bin/bash
 
 sudo rpm-ostree install --allow-inactive \
-  cockpit \
-  cockpit-dashboard \
-  cockpit-doc \
-  cockpit-docker \
-  cockpit-networkmanager \
-  cockpit-ostree \
-  cockpit-packagekit \
-  cockpit-selinux \
   docker \
-  docker-compose
+  docker-compose \
+  git-lfs \
+  kernel-tools \
+  powertop \
+  python3-speedtest-cli \
+  vim-enhanced
 cat bashrc >> ~/.bashrc
