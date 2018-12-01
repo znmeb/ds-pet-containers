1cc5665c10f4ddab0ab10a49c395d609ca8b4fcb
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-12-01 11:51:56 -0800

    version bump

--- containers/rstats-root-scripts/rstudio-server-preview.bash
+++ containers/rstats-root-scripts/rstudio-server-preview.bash
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 cd /usr/local/src
-export RSTUDIO_SERVER_VERSION=1.2.1139
+export RSTUDIO_SERVER_VERSION=1.2.1153
 wget -nc -q https://s3.amazonaws.com/rstudio-ide-build/server/debian9/x86_64/rstudio-server-${RSTUDIO_SERVER_VERSION}-amd64.deb
 gdebi -n rstudio-server-${RSTUDIO_SERVER_VERSION}-amd64.deb
