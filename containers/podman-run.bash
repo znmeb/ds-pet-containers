0924e738e2443ad84f6cf210146ce617ec17bbc9
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-02-07 11:32:34 -0800

    nuke existing containers first

--- containers/podman-run.bash
+++ containers/podman-run.bash
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+sudo podman rm --force postgis rstats pgadmin4
 sudo podman run --detach --name postgis --hostname postgis --publish 5439:5432 --env-file .env postgis:latest
 sudo podman run --detach --name rstats --hostname rstats --publish 8004:8004 --env-file .env rstats:latest
 sudo podman run --detach --name pgadmin4 --hostname pgadmin4 --publish 8686:80 --env-file .env docker.io/dpage/pgadmin4:latest
