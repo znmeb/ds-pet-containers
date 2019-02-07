362688568901f6d6b83e123439b3a3503c3cf5db
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-02-06 20:46:21 -0800

    opencpu port complete

--- containers/build-podman.bash
+++ containers/build-podman.bash
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-sudo podman build --tag rstats:latest --file Dockerfile.rstats .
+sudo podman build --tag rstats:latest --file Dockerfile.opencpu .
 sudo podman build --tag postgis:latest --file Dockerfile.postgis .
--- containers/build.bash
+++ containers/build.bash
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-sudo docker build --tag rstats:latest --file Dockerfile.rstats .
+sudo docker build --tag rstats:latest --file Dockerfile.opencpu .
 sudo docker build --tag postgis:latest --file Dockerfile.postgis .
