3c6870d379fd9e1ceb6b4a2cf5e1d6a050af7e9b
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-02-07 03:24:21 -0800

    rename for clarity

---
+++ containers/podman-run.bash
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+sudo podman run --detach --name postgis --hostname postgis --publish 5439:5432 --env-file .env postgis:latest
+sudo podman run --detach --name rstats --hostname rstats --publish 8004:8004 --env-file .env rstats:latest
+sudo podman run --detach --name pgadmin4 --hostname pgadmin4 --publish 8686:80 --env-file .env docker.io/dpage/pgadmin4:latest
--- containers/run.bash
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-sudo docker run --detach --name postgis --publish 5439:5432 --env-file .env postgis:latest
-sudo docker run --detach --name rstats --publish 8004:8004 --link postgis --env-file .env rstats:latest
-sudo docker run --detach --name pgadmin4 --publish 8686:80 --link postgis --env-file .env docker.io/dpage/pgadmin4:latest
