79c3cfe5f0b704b2f529f1b5ff15cf9a06e555d4
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-02-06 19:31:56 -0800

    different port for opencpu/rstudio

--- containers/run-podman.bash
+++ containers/run-podman.bash
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 sudo podman run --detach --name postgis --publish 5439:5432 --env-file .env postgis:latest
-sudo podman run --detach --name rstats --publish 8787:8787 --env-file .env rstats:latest
+sudo podman run --detach --name rstats --publish 8004:8004 --env-file .env rstats:latest
 sudo podman run --detach --name pgadmin4 --publish 8686:80 --env-file .env docker.io/dpage/pgadmin4:latest
