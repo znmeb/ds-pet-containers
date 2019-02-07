c595cb186806e784e6372fe159ce5b3eee417c48
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-02-07 03:13:40 -0800

    build "rstatsp" images with RStudio Preview

--- containers/build-podman.bash
+++ containers/build-podman.bash
@@ -1,4 +1,5 @@
 #! /bin/bash
 
 sudo podman build --tag rstats:latest --file Dockerfile.opencpu .
+sudo podman build --tag rstatsp:latest --file Dockerfile.opencpu-preview .
 sudo podman build --tag postgis:latest --file Dockerfile.postgis .
--- containers/build.bash
+++ containers/build.bash
@@ -1,4 +1,5 @@
 #! /bin/bash
 
 sudo docker build --tag rstats:latest --file Dockerfile.opencpu .
+sudo docker build --tag rstatsp:latest --file Dockerfile.opencpu-preview .
 sudo docker build --tag postgis:latest --file Dockerfile.postgis .
