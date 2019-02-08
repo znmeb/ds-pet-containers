288aec87b7f8100a0e6167775cc83f2ca700bf20
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-02-07 20:32:08 -0800

    fully-qualified image names

--- containers/podman-build.bash
+++ containers/podman-build.bash
@@ -1,5 +1,6 @@
 #! /bin/bash
 
-sudo podman build --tag rstats:latest --file Dockerfile.opencpu .
-sudo podman build --tag rstatsp:latest --file Dockerfile.opencpu-preview .
-sudo podman build --tag postgis:latest --file Dockerfile.postgis .
+sudo podman build --tag localhost/rstats:latest --file Dockerfile.opencpu .
+sudo podman build --tag localhost/rstatsp:latest --file Dockerfile.opencpu-preview .
+sudo podman build --tag localhost/postgis:latest --file Dockerfile.postgis .
+sudo podman images
