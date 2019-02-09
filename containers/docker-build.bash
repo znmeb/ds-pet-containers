ae2aa467f1fcd3f9a40dbc2d0ca18e53ab10471e
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-02-09 12:19:51 -0800

    pull the pgadmin4 image during build

--- containers/docker-build.bash
+++ containers/docker-build.bash
@@ -3,4 +3,5 @@
 sudo docker build --tag rstats:latest --file Dockerfile.opencpu .
 sudo docker build --tag rstatsp:latest --file Dockerfile.opencpu-preview .
 sudo docker build --tag postgis:latest --file Dockerfile.postgis .
+sudo docker pull docker.io/dpage/pgadmin4:latest
 sudo docker images
--- containers/podman-build.bash
+++ containers/podman-build.bash
@@ -3,4 +3,5 @@
 sudo podman build --tag localhost/rstats:latest --file Dockerfile.opencpu .
 sudo podman build --tag localhost/rstatsp:latest --file Dockerfile.opencpu-preview .
 sudo podman build --tag localhost/postgis:latest --file Dockerfile.postgis .
+sudo podman pull docker.io/dpage/pgadmin4:latest
 sudo podman images
