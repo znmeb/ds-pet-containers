76e3ac3e4dc69568315de7c76215504b3a35b935
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-02-08 21:18:54 -0800

    catch up with Linux version

--- containers/docker-run.cmd
+++ containers/docker-run.cmd
@@ -1,7 +1,8 @@
-docker rm --force postgis rstats pgadmin4
+podman rm --force `podman ps -aq`
+docker rm --force `docker ps -aq`
 docker network rm dspc
 docker network create --driver bridge dspc
 docker run --network dspc --detach --name postgis --publish 5439:5432 --env-file .env postgis:latest
-docker run --network dspc --detach --publish 8004:8004 --env-file .env rstats:latest
-docker run --network dspc --detach --publish 8686:80 --env-file .env docker.io/dpage/pgadmin4:latest
+docker run --network dspc --detach --name rstats --publish 8004:8004 --env-file .env rstats:latest
+docker run --network dspc --detach --name pgadmin4 --publish 8686:80 --env-file .env docker.io/dpage/pgadmin4:latest
 docker ps
