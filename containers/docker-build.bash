fcdfdcdf7c977eb1da5fdd4edbcb1b4816f9b86c
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-02-08 18:12:27 -0800

    list images after build

--- containers/docker-build.bash
+++ containers/docker-build.bash
@@ -3,3 +3,4 @@
 sudo docker build --tag rstats:latest --file Dockerfile.opencpu .
 sudo docker build --tag rstatsp:latest --file Dockerfile.opencpu-preview .
 sudo docker build --tag postgis:latest --file Dockerfile.postgis .
+sudo docker images
--- containers/docker-build.cmd
+++ containers/docker-build.cmd
@@ -1,3 +1,4 @@
 docker build --tag rstats:latest --file Dockerfile.opencpu .
 docker build --tag rstatsp:latest --file Dockerfile.opencpu-preview .
 docker build --tag postgis:latest --file Dockerfile.postgis .
+docker images
