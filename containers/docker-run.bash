b13c2bbc8c31128a0a4a0af25c48c4ab965c087f
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-02-08 21:15:25 -0800

    container names back; force-remove all containers

--- containers/docker-run.bash
+++ containers/docker-run.bash
@@ -1,9 +1,13 @@
 #! /bin/bash
 
-sudo docker rm --force postgis rstats pgadmin4
+sudo podman rm --force `sudo podman ps -aq`
+sudo docker rm --force `sudo docker ps -aq`
 sudo docker network rm dspc
 sudo docker network create --driver bridge dspc
-sudo docker run --network dspc --detach --name postgis --publish 5439:5432 --env-file .env postgis:latest
-sudo docker run --network dspc --detach --publish 8004:8004 --env-file .env rstats:latest
-sudo docker run --network dspc --detach --publish 8686:80 --env-file .env docker.io/dpage/pgadmin4:latest
+sudo docker run --network dspc --detach --name postgis --publish 5439:5432 --env-file .env \
+  postgis:latest
+sudo docker run --network dspc --detach --name rstats --publish 8004:8004 --env-file .env \
+  rstats:latest
+sudo docker run --network dspc --detach --name pgadmin4 --publish 8686:80 --env-file .env \
+  docker.io/dpage/pgadmin4:latest
 sudo docker ps
--- containers/podman-run.bash
+++ containers/podman-run.bash
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-sudo docker rm --force postgis rstats pgadmin4
-sudo podman rm --force postgis rstats pgadmin4
+sudo podman rm --force `sudo podman ps -aq`
+sudo docker rm --force `sudo docker ps -aq`
 #
 # podman doesn't have "networks" in the same sense as "docker network". It comes with a single
 # default CNI network: 10.88.0.0/16.
@@ -9,10 +9,10 @@ sudo podman rm --force postgis rstats pgadmin4
 # So for the other two containers to refer to the "postgis" container by name, we have to party
 # like it's 1982 - assign it a static IP address and create an "/etc/hosts" file pointing to it
 # in the other two containers. See "man podman-run" for details.
-sudo podman run --detach --ip "10.88.64.128" --publish 5439:5432 --env-file .env \
+sudo podman run --detach --name postgis --ip "10.88.64.128" --publish 5439:5432 --env-file .env \
   localhost/postgis:latest
-sudo podman run --detach --add-host "postgis:10.88.64.128" --publish 8004:8004 --env-file .env \
+sudo podman run --detach --name rstats --add-host "postgis:10.88.64.128" --publish 8004:8004 --env-file .env \
   localhost/rstats:latest
-sudo podman run --detach --add-host "postgis:10.88.64.128" --publish 8686:80 --env-file .env \
+sudo podman run --detach --name pgadmin4 --add-host "postgis:10.88.64.128" --publish 8686:80 --env-file .env \
   docker.io/dpage/pgadmin4:latest
 sudo podman ps
