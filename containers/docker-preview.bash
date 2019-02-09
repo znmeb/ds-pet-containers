585dd49bad2cc369e1ed6bda03ef634b1b052eda
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-02-09 12:28:38 -0800

    use the RStudio Server Preview image

---
+++ containers/docker-preview.bash
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+sudo podman rm --force postgis pgadmin4 rstats
+sudo docker rm --force postgis pgadmin4 rstats
+sudo docker network rm dspc
+sudo docker network create --driver bridge dspc
+sudo docker run --network dspc --detach --env-file .env \
+  --hostname postgis --name postgis --publish 5439:5432 \
+  postgis:latest
+sudo docker run --network dspc --detach --env-file .env \
+  --hostname rstats --name rstats --publish 8004:8004 \
+  rstatsp:latest
+sudo docker run --network dspc --detach --env-file .env \
+  --hostname pgadmin4 --name pgadmin4 --publish 8686:80 \
+  docker.io/dpage/pgadmin4:latest
+sudo docker ps
+sudo docker exec -u rstats rstats /home/rstats/scripts/is_postgis_there.R
+sudo docker exec -it -u postgres pgadmin4 sh -c 'PGPASSWORD=$POSTGRES_PASSWORD psql --host postgis -l'
---
+++ containers/podman-preview.bash
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+sudo podman rm --force postgis pgadmin4 rstats
+sudo docker rm --force postgis pgadmin4 rstats
+
+# podman doesn't have "networks" in the same sense as "docker network". It comes with a single
+# default CNI network: 10.88.0.0/16.
+
+# So for the other two containers to refer to the "postgis" container by name, we have to party
+# like it's 1982 - assign it a static IP address and create an "/etc/hosts" file pointing to it
+# in the other two containers. See "man podman-run" for details.
+
+sudo podman run --detach --env-file .env \
+  --hostname postgis --name postgis --ip 10.88.64.128 --publish 5439:5432 \
+  --add-host postgis:10.88.64.128 --add-host pgadmin4:10.88.64.130 --add-host rstats:10.88.64.129 \
+  localhost/postgis:latest
+sudo podman run --detach --env-file .env \
+  --hostname rstats --name rstats --ip 10.88.64.129 --publish 8004:8004 \
+  --add-host postgis:10.88.64.128 --add-host pgadmin4:10.88.64.130 --add-host rstats:10.88.64.129 \
+  localhost/rstatsp:latest
+sudo podman run --detach --env-file .env \
+  --hostname pgadmin4 --name pgadmin4 --ip 10.88.64.130 --publish 8686:80 \
+  --add-host postgis:10.88.64.128 --add-host pgadmin4:10.88.64.130 --add-host rstats:10.88.64.129 \
+  docker.io/dpage/pgadmin4:latest
+sudo podman ps
+sudo podman exec -u rstats rstats /home/rstats/scripts/is_postgis_there.R
+sudo podman exec -it -u postgres pgadmin4 sh -c 'PGPASSWORD=$POSTGRES_PASSWORD psql --host postgis -l'
