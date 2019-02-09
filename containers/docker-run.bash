d619695e2f7befe4690eea22a35bb9e469cda50d
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-02-08 18:26:53 -0800

    only the PostGIS container needs a name!

--- containers/docker-run.bash
+++ containers/docker-run.bash
@@ -1,7 +1,9 @@
 #! /bin/bash
 
 sudo docker rm --force postgis rstats pgadmin4
-sudo podman rm --force postgis rstats pgadmin4
-sudo docker run --detach --name postgis --hostname postgis --publish 5439:5432 --env-file .env postgis:latest
-sudo docker run --detach --name rstats --hostname rstats --publish 8004:8004 --env-file .env rstats:latest
-sudo docker run --detach --name pgadmin4 --hostname pgadmin4 --publish 8686:80 --env-file .env docker.io/dpage/pgadmin4:latest
+sudo docker network rm dspc
+sudo docker network create --driver bridge dspc
+sudo docker run --network dspc --detach --name postgis --publish 5439:5432 --env-file .env postgis:latest
+sudo docker run --network dspc --detach --publish 8004:8004 --env-file .env rstats:latest
+sudo docker run --network dspc --detach --publish 8686:80 --env-file .env docker.io/dpage/pgadmin4:latest
+sudo docker ps
