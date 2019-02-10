a0b729b628ed74070873349817bb390993a2e925
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-02-09 22:29:38 -0800

    pre-pull images to save time

---
+++ containers/docker-pull.bash
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+docker pull docker.io/library/postgres:10
+docker pull docker.io/dpage/pgadmin4:latest
+docker pull docker.io/opencpu/rstudio:latest
+docker images
---
+++ containers/docker-pull.cmd
@@ -0,0 +1,4 @@
+docker pull docker.io/library/postgres:10
+docker pull docker.io/dpage/pgadmin4:latest
+docker pull docker.io/opencpu/rstudio:latest
+docker images
