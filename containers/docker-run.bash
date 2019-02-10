8e72bffbc296f17366707742830a5afd47d04243
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-02-09 23:32:05 -0800

    more sudos removed

--- containers/docker-run.bash
+++ containers/docker-run.bash
@@ -1,18 +1,18 @@
 #! /bin/bash
 
 sudo podman rm --force postgis pgadmin4 rstats
-sudo docker rm --force postgis pgadmin4 rstats
-sudo docker network rm dspc
-sudo docker network create --driver bridge dspc
-sudo docker run --network dspc --detach --env-file .env \
+docker rm --force postgis pgadmin4 rstats
+docker network rm dspc
+docker network create --driver bridge dspc
+docker run --network dspc --detach --env-file .env \
   --hostname postgis --name postgis --publish 5439:5432 \
   postgis:latest
-sudo docker run --network dspc --detach --env-file .env \
+docker run --network dspc --detach --env-file .env \
   --hostname rstats --name rstats --publish 8004:8004 \
   rstats:latest
-sudo docker run --network dspc --detach --env-file .env \
+docker run --network dspc --detach --env-file .env \
   --hostname pgadmin4 --name pgadmin4 --publish 8686:80 \
   docker.io/dpage/pgadmin4:latest
-sudo docker ps
-sudo docker exec -u rstats rstats /home/rstats/scripts/is_postgis_there.R
-sudo docker exec -it -u postgres pgadmin4 sh -c 'PGPASSWORD=$POSTGRES_PASSWORD psql --host postgis -l'
+docker ps
+docker exec -u rstats rstats /home/rstats/scripts/is_postgis_there.R
+docker exec -it -u postgres pgadmin4 sh -c 'PGPASSWORD=$POSTGRES_PASSWORD psql --host postgis -l'
