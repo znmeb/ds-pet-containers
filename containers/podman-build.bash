029ffd92fa2af6f6f78d40951e765543f12185c7
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-02-11 14:49:10 -0800

    moved podman stuff to new branch

--- containers/podman-build.bash
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-sudo podman build --tag localhost/rstats:latest --file Dockerfile.opencpu .
-sudo podman build --tag localhost/rstatsp:latest --file Dockerfile.opencpu-preview .
-sudo podman build --tag localhost/postgis:latest --file Dockerfile.postgis .
-sudo podman images
