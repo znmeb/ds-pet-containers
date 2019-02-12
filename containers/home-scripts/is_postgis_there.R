c0f6164b2776b7ddca98b7b99d67f2b7405c8d77
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-02-12 03:09:31 -0800

    use localhost instead of `postgis`

--- containers/home-scripts/is_postgis_there.R
+++ containers/home-scripts/is_postgis_there.R
@@ -4,7 +4,7 @@ for (itry in 1:10) {
   if (
     print(DBI::dbCanConnect(
       drv = RPostgres::Postgres(),
-      host = "postgis",
+      host = "localhost",
       port = 5432,
       user = "postgres",
       password = Sys.getenv("POSTGRES_PASSWORD"),
