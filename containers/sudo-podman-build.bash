fe252899a5be72276f40777f7a23cf2dc26af3b2
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-02-13 01:22:57 -0800

    pull the pgAdmin image

--- containers/sudo-podman-build.bash
+++ containers/sudo-podman-build.bash
@@ -3,4 +3,5 @@
 sudo podman build --tag rstats:latest --file Dockerfile.opencpu .
 sudo podman build --tag rstatsp:latest --file Dockerfile.opencpu-preview .
 sudo podman build --tag postgis:latest --file Dockerfile.postgis .
+sudo podman pull docker.io/dpage/pgadmin4:latest
 sudo podman images
