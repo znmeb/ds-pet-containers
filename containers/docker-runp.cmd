80fbdc008be1dcb9bb9bd291a66cafa5aa973fa5
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-02-12 21:06:01 -0800

    cleanup

--- containers/docker-runp.cmd
+++ containers/docker-runp.cmd
@@ -1,7 +1,9 @@
 docker rm --force postgis pgadmin4 rstats
-docker run --network host --detach --env-file .env --name postgis --publish 5432:5432 postgis:latest
-docker run --network host --detach --env-file .env --name rstats --publish 8004:8004 rstatsp:latest
-docker run --network host --detach --env-file .env --name pgadmin4 --publish 8686:8686 docker.io/dpage/pgadmin4:latest
+docker network rm dspc
+docker network create --driver bridge dspc
+docker run --network dspc --detach --env-file .env --hostname postgis --name postgis --publish 5432:5432 postgis:latest
+docker run --network dspc --detach --env-file .env --hostname rstats --name rstats --publish 8004:8004 rstatsp:latest
+docker run --network dspc --detach --env-file .env --hostname pgadmin4 --name pgadmin4 --publish 8686:8686 docker.io/dpage/pgadmin4:latest
 docker ps
 docker exec -u rstats rstats /home/rstats/scripts/is_postgis_there.R
-docker exec -it -u postgres pgadmin4 sh -c 'PGPASSWORD=$POSTGRES_PASSWORD psql --host localhost -l'
+docker exec -it -u postgres pgadmin4 sh -c "PGPASSWORD=\$POSTGRES_PASSWORD psql --host postgis -l"
