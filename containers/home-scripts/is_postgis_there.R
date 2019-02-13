89055a22a1d0bd79dbe7db3b12dfcda5993fc29d
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-02-13 01:57:51 -0800

    back to named containers

--- containers/home-scripts/is_postgis_there.R
+++ containers/home-scripts/is_postgis_there.R
@@ -4,7 +4,7 @@ for (itry in 1:10) {
   if (
     print(DBI::dbCanConnect(
       drv = RPostgres::Postgres(),
-      host = "localhost",
+      host = "postgis",
       port = 5432,
       user = "postgres",
       password = Sys.getenv("POSTGRES_PASSWORD"),
