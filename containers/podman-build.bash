cbe7b666ee13bf27100a6e2f46a9c3c6236e206f
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-02-17 00:41:47 -0800

    ditch rootless again

--- containers/podman-build.bash
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-podman build --tag rstats:latest --file Dockerfile.opencpu .
-podman build --tag rstatsp:latest --file Dockerfile.opencpu-preview .
-podman build --tag postgis:latest --file Dockerfile.postgis .
-podman pull docker.io/dpage/pgadmin4:latest
-podman images
