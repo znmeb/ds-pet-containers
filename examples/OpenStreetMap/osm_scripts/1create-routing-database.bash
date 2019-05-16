6651baaeb601269b2fd123a8909753d0825171ad
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-05-16 01:08:29 -0700

    clean up old stuff from Hack Oregon - it's still there!

--- examples/OpenStreetMap/osm_scripts/1create-routing-database.bash
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-echo "Creating fresh database 'osm_routing'; ignore error if it doesn't exist"
-dropdb osm_routing || true # don't quit if database doesn't exist
-createdb osm_routing
-echo "Creating extensions"
-psql -d osm_routing -c "CREATE EXTENSION postgis CASCADE;"
-psql -d osm_routing -c "CREATE EXTENSION pgrouting CASCADE;"
-psql -d osm_routing -c "CREATE EXTENSION hstore CASCADE;"
