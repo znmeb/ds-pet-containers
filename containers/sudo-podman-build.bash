0b8b96a72e7a039cdbfb615e823270776e134a01
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-05-16 00:22:15 -0700

    ditching podman - not on Clear Linux

--- containers/sudo-podman-build.bash
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-sudo podman build --tag rstats:latest --file Dockerfile.opencpu .
-sudo podman build --tag rstatsp:latest --file Dockerfile.opencpu-preview .
-sudo podman build --tag postgis:latest --file Dockerfile.postgis .
-sudo podman pull docker.io/dpage/pgadmin4:latest
-sudo podman images
