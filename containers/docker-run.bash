39e61ebee86e87048697d60293815c8188b7e0eb
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-05-22 12:11:07 -0700

    re-order Docker run commands

--- containers/docker-run.bash
+++ containers/docker-run.bash
@@ -9,14 +9,14 @@ sudo docker network rm silver-potato
 sudo docker network create --driver bridge silver-potato
 
 echo "running containers"
-sudo docker run --network silver-potato --detach --env-file .env \
-  --hostname postgis --name postgis --publish 5439:5432 \
+sudo docker run --detach --env-file .env \
+  --network silver-potato --hostname postgis --name postgis --publish 5439:5432 \
   postgis:latest
-sudo docker run --network silver-potato --detach --env-file .env \
-  --hostname rstats --name rstats --publish 8787:8787 \
+sudo docker run --detach --env-file .env \
+  --network silver-potato --hostname rstats --name rstats --publish 8787:8787 \
   rstats:latest
-sudo docker run --network silver-potato --detach --env-file .env \
-  --hostname pgadmin4 --name pgadmin4 --publish 8686:8686 \
+sudo docker run --detach --env-file .env \
+  --network silver-potato --hostname pgadmin4 --name pgadmin4 --publish 8686:8686 \
   docker.io/dpage/pgadmin4:latest
 sudo docker ps
 sudo docker exec -u rstudio rstats /home/rstudio/scripts/is_postgis_there.R
