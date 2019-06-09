13e42d12b9c5583c717b03c21d2677a50dd1d9e8
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-06-09 09:08:36 -0700

    fix typo

--- containers/rstats-scripts/start-jupyter.bash
+++ containers/rstats-scripts/start-jupyter.bash
@@ -4,5 +4,5 @@
 conda activate r-tensorflow
 echo "Set a notebook password"
 jupyter notebook password
-echo "Browse to 'http://loaclhost:8888'"
+echo "Browse to 'http://localhost:8888'"
 jupyter notebook --ip=0.0.0.0 --no-browser
